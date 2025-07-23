de53049b86ffeb7e6abc4ab979abf5cf8e13453e
madhirekalyansathwik <madhirekalyansathwik@gmail.com>
2025-07-23 21:59:55 +0530

    learnt operations

--- 01.basics/conversionOperation.js
+++ 01.basics/conversionOperation.js
@@ -1,7 +1,7 @@
 let score = "33"
 
-console.log(typeof(score))
-console.log(typeof score)
+// console.log(typeof(score))
+// console.log(typeof score)
 
 // line 3 and 4 shows we can write it as normally, as a method
 // Both of them works same.
@@ -10,27 +10,27 @@ console.log(typeof score)
 // do the operation in numbers then\
 
 let valueInNumber = Number(score) // we have to write in Capital despite we got the result in small letters
-console.log(typeof valueInNumber)
+// console.log(typeof valueInNumber)
 
 // what if
 score = "33abc"
 valueInNumber = Number(score) // we have to write in Capital despite we got the result in small letters
-console.log(typeof valueInNumber) // how did this give number as type??
-console.log(valueInNumber) // gives us NaN => Not a Number
+// console.log(typeof valueInNumber) // how did this give number as type??
+// console.log(valueInNumber) // gives us NaN => Not a Number
 // 33abc is not a number, so this is how we can check whether a value in number of NaN
 // so we can't solely rely on types, we must be cautious
 
 score = null
-console.log(score)
-console.log(typeof score)
+// console.log(score)
+// console.log(typeof score)
 
 score = undefined
-console.log(score)
-console.log(typeof score)
+// console.log(score)
+// console.log(typeof score)
 
 score = Number('kalyan')
-console.log(score)
-console.log(typeof score)
+// console.log(score)
+// console.log(typeof score)
 
 // from these examples we can see, conversion of:
 // 33 => 33
@@ -44,5 +44,24 @@ console.log(typeof score)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// Operations
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+let str1 = "hello"
+let str2 = "hitesh"
+
+let str3 = str1 +" "+ str2
+console.log(str3)
+
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+// if string is first everything gets string converted
+// if string is at last, then first calculation takes place
+// then string conversion at last.
