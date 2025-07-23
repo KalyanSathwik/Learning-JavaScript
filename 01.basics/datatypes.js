d009cfa54370a64d50986ad56351256c53fd8118
madhirekalyansathwik <madhirekalyansathwik@gmail.com>
2025-07-23 19:46:32 +0530

    studied second lesson

---
+++ 01.basics/datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all js code as newer version
+
+// alert( 3 + 3)
+// syntax for alert is different, as we are using node 
+// but not a browser.
+
+// code readability :
+console.log(3+3); console.log("Kalyan") // bad code readability
+// Another one : 
+console.log(3+
+    3)
+console.log('Sathwik')
+
+// Good one : 
+console.log("Madhire")
+console.log(2+5)
+
+//Datatypes :
+/*
+    number
+    bigint
+    string
+    boolean
+    null => stand alone value
+    undefined =>
+    symbol => unique
+
+    object
+*/
+
+console.log(typeof("kalyan"))
